31551de28c4b2e1facc8c2ceff6d9788beeff3c1
developeter <mracspeter@gmail.com>
2017-02-21 14:13:37 +0000

    Create getjson and the new-quote button

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
+
+var getQuote = function (data) {
+    $('.quote-text').html(data.quoteText);
+    if (data.quoteAuthor === '') {
+        data.quoteAuthor = 'Unknown';
+    }
+    $('.quote-author').html(data.quoteAuthor);
+};
+$(document).ready(function () {
+    $.getJSON(url, getQuote, 'jsonp');
+});
+
+$('#new-quote').click(function () {
+    $.getJSON(url, getQuote, 'jsonp');
+});
+
 
