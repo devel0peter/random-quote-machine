e644bb275f7ff2d46e8cf01581d6e15b1d10072a
developeter <mracspeter@gmail.com>
2017-02-21 15:54:05 +0000

    Create tweet button

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=
 
 var getQuote = function (data) {
     $('.quote-text').html(data.quoteText);
+    var tweet = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor +' @mracspeter';
     if (data.quoteAuthor === '') {
         data.quoteAuthor = 'Unknown';
     }
     $('.quote-author').html(data.quoteAuthor);
+    $('#tweet-it').attr('href', tweet);
 };
 $(document).ready(function () {
     $.getJSON(url, getQuote, 'jsonp');
