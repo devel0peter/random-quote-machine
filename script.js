6895b269c6022e6f104baedc481965a911845aab
developeter <mracspeter@gmail.com>
2017-02-22 09:26:23 +0000

    Finish style

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=
 
 var getQuote = function (data) {
     $('.quote-text').html(data.quoteText);
-    var tweet = 'https://twitter.com/intent/tweet?text=' + data.quoteText + ' Author ' + data.quoteAuthor +' @mracspeter';
+    var tweet = 'https://twitter.com/intent/tweet?text=' + '"' + data.quoteText + '"  - ' + data.quoteAuthor;
     if (data.quoteAuthor === '') {
         data.quoteAuthor = 'Unknown';
     }
-    $('.quote-author').html(data.quoteAuthor);
+    $('.quote-author').html(" - " + data.quoteAuthor + " - ");
     $('#tweet-it').attr('href', tweet);
 };
+
 $(document).ready(function () {
     $.getJSON(url, getQuote, 'jsonp');
 });
 
-$('#new-quote').click(function () {
+$('#new-quote-btn').click(function () {
     $.getJSON(url, getQuote, 'jsonp');
 });
+
 
 
